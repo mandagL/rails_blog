f12c93e61a0a91d5ddcbea484b454cae37daee2c
Ludovic Mandagot <ludovic.mandagot@epitech.eu>
2020-06-18 16:01:23 +0200

    CRUD articles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  def index
+    @article = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      redirect_to articles_url
+    else
+      redirect_to articles_url
+    end
+
+  end
+  
+
+  def create
+    @article = Article.new(article_params)
+  
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
